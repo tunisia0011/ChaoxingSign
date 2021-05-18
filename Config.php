92d06590b03d5b8e86ffc60a7d98023807d2a5f0
tunisia0011 <82506259+tunisia0011@users.noreply.github.com>
2021-05-18 15:01:10 +0800

    Update Config.php

--- Config.php
+++ Config.php
@@ -6,19 +6,19 @@ define("SERVER_CHAN_STATE", true);
 $config = [
     //Server酱：http://sc.ftqq.com/
     'SERVER_CHAN' => [
-        '这里填你的超星账号 1' => [
+        '${USERNAME1}' => [
             'state' => true,//是否启用 Server酱 通知，true: 启用 false: 不启用
 
             //填入该值，表示你使用的是新版推送通道。旧版推送通道将于 2021年4月 下线
             //SendKey 获取地址：https://sct.ftqq.com/sendkey
-            'SendKey' => '',
+            'SendKey' => '${SENDKEY}',
         ],
-        '这里填你的超星账号 2' => [
+        '${USERNAME2}' => [
             'state' => true,//是否启用 Server酱 通知，true: 启用 false: 不启用
 
             //填入该值，表示你使用的是新版推送通道。旧版推送通道将于 2021年4月 下线
             // SendKey 获取地址：https://sct.ftqq.com/sendkey
-            'SendKey' => '',
+            'SendKey' => '${SENDKEY}',
         ],
         //... 多账号部署
     ],
